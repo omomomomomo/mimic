cbffddf044ca2430822abef14d1882d9e07aa284
omori <omoriyosuke0331@gmail.com>
2021-06-04 01:42:27 +0900

    first19

--- style_transfer/migrations/0001_initial.py
+++ style_transfer/migrations/0001_initial.py
@@ -14,10 +14,11 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Style',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True,
+                 primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=100)),
-                ('image', models.ImageField(upload_to='styles/images')),
-                ('model', models.FileField(upload_to='styles/models')),
+                ('image', models.ImageField(upload_to='media/styles/images')),
+                ('model', models.FileField(upload_to='media/styles/models')),
             ],
         ),
     ]
