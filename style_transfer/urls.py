ae61df30b40eb0bb7604a55c0cdf49e566602e62
omori <omoriyosuke0331@gmail.com>
2021-06-04 12:33:54 +0900

    second2

--- style_transfer/urls.py
+++ style_transfer/urls.py
@@ -14,4 +14,4 @@ urlpatterns = [
     path('stylize/', views.apply_style_transfer),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
